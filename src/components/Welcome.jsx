f18bb89fa59aa83a49ffe257c448ac713b9238a1
edtrade <edtrade88@gmail.com>
2021-12-30 00:13:05 -0500

    abi & connect metamask

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,6 +3,7 @@ import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 import { Loader } from ".";
+import { TransactionContext } from "../context/TransactionContext";
 
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -19,6 +20,13 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
   );
 
 const Welcome = () => {
+
+    const { currentAccount } = useContext(TransactionContext)
+    
+    const truncate = (str, n)=>{
+        return (str.length > n) ? str.substr(0, n-1) + '...' : str;
+      };
+      
 
     const connectWallet = ()=>{
         alert()
@@ -84,7 +92,7 @@ const Welcome = () => {
                     </div>
                     <div>
                         <p className="text-white font-light text-sm">
-                            0x093....8FG7a8s
+                            {truncate(currentAccount,30)}
                         </p>
                         <p className="text-white font-semibold text-lg mt-1">
                         Ethereum
